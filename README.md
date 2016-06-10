8d1fb2d3dd2b10a76e3156988a537737f8bf63a1
Matt Smith <runner_28@hotmail.com>
2016-06-09 21:03:51 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # generator-simple-project
-A Yeoman generator that scaffolds out a simple project
+
+A tiny [Yeoman](http://yeoman.io) that quickly scaffolds out a simple project.
+
+
+## Getting Started
+
+1. Install the generator by running: `npm install -g generator-simple-project`
+2. Run: `yo simple`
+3. Run: `grunt` to start a LiveReload server on port `9000`
+
+
+## What's Included
+
+The simple generator contains a `index.html`, `main.css`, and `main.js` file. There's also a LiveReload server so you can see your changes quickly.
+
+
+## License
+
+[MIT License](https://github.com/AllThingsSmitty/generator-simple-project/blob/master/LICENSE)
---
+++ app/index.js
@@ -0,0 +1,36 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+
+
+var SimpleGenerator = module.exports = function SimpleGenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(SimpleGenerator, yeoman.generators.Base);
+
+SimpleGenerator.prototype.app = function app() {
+  this.template('_bower.json', 'bower.json');
+  this.template('_Gruntfile.js', 'Gruntfile.js');
+  this.template('_index.html', 'index.html');
+  this.template('_package.json', 'package.json');
+  this.copy('main.css', 'css/main.css');
+  this.copy('main.js', 'js/main.js');
+};
+
+SimpleGenerator.prototype.projectfiles = function projectfiles() {
+  this.copy('editorconfig', '.editorconfig');
+  this.copy('jshintrc', '.jshintrc');
+};
+
+SimpleGenerator.prototype.gitfiles = function gitfiles() {
+  this.copy('gitattributes', '.gitattributes');
+  this.copy('gitignore', '.gitignore');
+};
